840d743cf1d7f52c8483921cc2d5da7f11425104
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-02-01 19:32:49 +0300

    Add ItemMapper class

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,30 @@
+package ru.practicum.shareit.item;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ItemMapper {
+    public static ItemDto convertToDto(Item item) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setId(item.getItemId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.isAvailable());
+        itemDto.setOwner(item.getOwner());
+        itemDto.setRequest(item.getRequest());
+        return itemDto;
+    }
+
+    public static Item convertToEntity(ItemDto itemDto) {
+        Item item = new Item();
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        item.setOwner(itemDto.getOwner());
+        item.setRequest(itemDto.getRequest());
+        return item;
+    }
+}
