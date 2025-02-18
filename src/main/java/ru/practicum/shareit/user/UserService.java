6436265257e75baac980b5e2ff9a46e62f122654
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-02-18 17:06:06 +0300

    UserService: add methods

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -1,4 +1,17 @@
 package ru.practicum.shareit.user;
 
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.Collection;
+
 public interface UserService {
+    Collection<UserDto> getUsers();
+
+    UserDto getUserById(Long userId);
+
+    UserDto createUser(UserDto userDto);
+
+    UserDto updateUser(Long userId, UserDto userDto);
+
+    void deleteUser(Long userId);
 }
