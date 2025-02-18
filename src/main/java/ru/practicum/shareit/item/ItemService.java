a0aac19a7dcd8c760a81c458552c7571967b92c8
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-02-18 17:18:30 +0300

    ItemService: add methods

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -1,4 +1,25 @@
 package ru.practicum.shareit.item;
 
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+
+import java.util.Collection;
+
 public interface ItemService {
+    Collection<ItemDto> findByUserId(long userId);
+
+    ItemDto getItemById(long itemId);
+
+    ItemDto addItem(ItemDto itemDto, long userId);
+
+    ItemDto updateItem(ItemUpdateDto itemUpdateDto, long userId, long itemId);
+
+    void deleteItem(long userId, long itemId);
+
+    Collection<ItemDto> searchItems(String text);
+
+    CommentDto addComment(Long itemId, Long userId, CommentDto commentDto);
+
+    void updateBookingsForItem(Long itemId);
 }
