59ce54fdc3e7c54c164dacf4f536545fdc7c1ff2
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-02-18 17:15:16 +0300

    ItemRepository: now extends JpaRepository

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -1,18 +1,22 @@
 package ru.practicum.shareit.item;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.Collection;
-import java.util.Optional;
 
-public interface ItemRepository {
-    Collection<Item> findByUserId(long userId);
+public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    Optional<Item> findById(long itemId);
+    Collection<Item> findByOwnerId(Long userId);
 
-    Item save(Item item);
+    @Transactional
+    void deleteByOwnerIdAndItemId(Long ownerId, Long itemId);
 
-    void deleteByUserIdAndItemId(long userId, long itemId);
-
+    @Query("SELECT i FROM Item i WHERE " +
+            "(LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
+            "LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%'))) AND " +
+            "i.isAvailable = true")
     Collection<Item> searchItems(String text);
 }
