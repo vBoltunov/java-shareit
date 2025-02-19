b89bbb6fd20a59a9d1b7339e16911fdd0a464564
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-02-19 14:03:41 +0300

    Add PathConstants class

---
+++ src/main/java/ru/practicum/shareit/util/PathConstants.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.util;
+
+public class PathConstants {
+    public static final String ITEM_ID_PATH = "/{item-id}";
+    public static final String BOOKING_ID_PATH = "/{booking-id}";
+    public static final String USER_ID_PATH = "/{user-id}";
+}
